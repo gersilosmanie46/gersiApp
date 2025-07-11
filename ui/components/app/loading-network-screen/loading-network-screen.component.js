cd3bb160103b882d5b836026c7c393b0067fdbc3
gersilosmanie46 <147956683+gersilosmanie46@users.noreply.github.com>
2025-07-11 03:20:30 +0200

    Create loading-network-screen.component.js

---
+++ ui/components/app/loading-network-screen/loading-network-screen.component.js
@@ -0,0 +1,28 @@
+import React, { PureComponent } from 'react';
+import PropTypes from 'prop-types';
+import LoadingScreen from '../../ui/loading-screen';
+import { SECOND } from '../../../../shared/constants/time';
+import {
+  DEPRECATED_NETWORKS,
+  NETWORK_TYPES,
+} from '../../../../shared/constants/network';
+import Popover from '../../ui/popover/popover.component';
+import {
+  ButtonPrimary,
+  ButtonSecondary,
+  Icon,
+  IconName,
+  IconSize,
+  Text,
+} from '../../component-library';
+import {
+  DISPLAY,
+  IconColor,
+  TextAlign,
+} from '../../../helpers/constants/design-system';
+
+class LoadingNetworkScreen extends PureComponent {
+  
+}
+
+export default LoadingNetworkScreen;
